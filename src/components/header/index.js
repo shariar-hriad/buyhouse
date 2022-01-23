b3b3fdc6177082c713214f0c3d336e2e486dcda8
riad <shariarhossainriad@gmail.com>
2022-01-23 11:06:20 +0600

    mobile menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useRef, useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
 import { GiRotaryPhone } from 'react-icons/gi';
 import logo from '/public/logoc.png';
 
@@ -29,9 +29,10 @@ export default function Header() {
         ref={headerRef}
         className={`${
           fixed ? 'bg-white' : ''
-        } fixed z-50 px-8 w-full flex items-center transition-all font-asap`}
+        } fixed z-50 px-8 pt-3 w-full flex items-center transition-all font-asap`}
       >
-        <div className="flex w-full justify-between">
+        <div className="flex w-full justify-between relative">
+          {/* logo */}
           <div className="cursor-pointer w-24 md:w-52">
             <Link href="/">
               <a>
@@ -43,6 +44,7 @@ export default function Header() {
               </a>
             </Link>
           </div>
+          {/* desktop menu */}
           <nav className="hidden xl:block flex-1 w-full mr-5">
             <ul
               className={`${
@@ -92,6 +94,7 @@ export default function Header() {
               </Link>
             </ul>
           </nav>
+          {/* mobile menu button */}
           <div className={`flex-1 pt-3 md:pt-10 flex justify-end  xl:hidden`}>
             <div
               className="cursor-pointer text-2xl text-aqua"
@@ -100,69 +103,60 @@ export default function Header() {
               <FaBars />
             </div>
           </div>
+          {/* mobile menu */}
+          <div
+            className={`absolute z-50 top-full xl:hidden mt-4 bg-mirage w-full ${
+              toggle ? 'h-auto opacity-100' : 'h-0 opacity-0'
+            } transition-all p-3.5 border-t-4 border-t-aqua`}
+          >
+            <ul
+              className={`font-bold text-white flex mt-4 flex-col items-start h-auto gap-y-7 pl-3.5 text-sm capitalize text-left`}
+            >
+              <li className="transition-all hover:text-iron-gray">
+                <Link href="/">
+                  <a>sell your house</a>
+                </Link>
+              </li>
+              <li className="transition-all hover:text-iron-gray">
+                <Link href="/blogs">
+                  <a>how it works</a>
+                </Link>
+              </li>
+              <li className="transition-all hover:text-iron-gray">
+                <Link href="/blogs">
+                  <a>compare</a>
+                </Link>
+              </li>
+              <li className="transition-all hover:text-iron-gray">
+                <Link href="/blogs">
+                  <a>about us</a>
+                </Link>
+              </li>
+              <li className="transition-all hover:text-iron-gray">
+                <Link href="/blogs">
+                  <a>services</a>
+                </Link>
+              </li>
+              <li className="transition-all hover:text-iron-gray">
+                <Link href="/blogs">
+                  <a>blog</a>
+                </Link>
+              </li>
+              <li className="transition-all hover:text-iron-gray">
+                <Link href="/blogs">
+                  <a>contact us</a>
+                </Link>
+              </li>
+              <Link href="tel:(503) 610-8828">
+                <a className="flex items-center flex-nowrap bg-aqua text-white font-medium rounded-full px-5 py-3 cursor-pointer">
+                  <GiRotaryPhone />
+                  (503) 610-8828
+                </a>
+              </Link>
+            </ul>
+          </div>
         </div>
       </header>
-      {/* mobile menu */}
-      <aside
-        className={`fixed z-50 bg-black bg-opacity-70 top-0  bottom-0 ${
-          toggle ? 'right-0' : '-right-full '
-        } w-full transition-all`}
-      >
-        <div className="bg-white w-4/5 h-full absolute right-0 flex flex-col items-center justify-center">
-          <ul className="font-bold flex flex-col gap-y-5 capitalize text-center">
-            <li className="border-b-2 py-3 border-transparent hover:border-b-aqua transition-all">
-              <Link href="/">
-                <a>sell your house</a>
-              </Link>
-            </li>
-            <li className="border-b-2 py-3 border-transparent hover:border-b-aqua transition-all">
-              <Link href="/blogs">
-                <a>how it works</a>
-              </Link>
-            </li>
-            <li className="border-b-2 py-3 border-transparent hover:border-b-aqua transition-all">
-              <Link href="/blogs">
-                <a>compare</a>
-              </Link>
-            </li>
-            <li className="border-b-2 py-3 border-transparent hover:border-b-aqua transition-all">
-              <Link href="/blogs">
-                <a>about us</a>
-              </Link>
-            </li>
-            <li className="border-b-2 py-3 border-transparent hover:border-b-aqua transition-all">
-              <Link href="/blogs">
-                <a>services</a>
-              </Link>
-            </li>
-            <li className="border-b-2 py-3 border-transparent hover:border-b-aqua transition-all">
-              <Link href="/blogs">
-                <a>blog</a>
-              </Link>
-            </li>
-            <li className="border-b-2 py-3 border-transparent hover:border-b-aqua transition-all">
-              <Link href="/blogs">
-                <a>contact us</a>
-              </Link>
-            </li>
-            <Link href="tel:(503) 610-8828">
-              <a className="flex items-center hover:underline flex-nowrap bg-aqua text-white font-medium rounded-full px-5 py-3 cursor-pointer">
-                <GiRotaryPhone />
-                (503) 610-8828
-              </a>
-            </Link>
-          </ul>
-        </div>
-        {/* close menu */}
-        <div className="absolute z-50 left-4 top-8">
-          <button
-            className="text-white text-3xl cursor-pointer bg-aqua p-1.5 rounded-md"
-            onClick={() => setToggle(!toggle)}
-          >
-            <FaTimes />
-          </button>
-        </div>
-      </aside>
     </>
   );
 }
